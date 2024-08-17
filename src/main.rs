4da6e8d1f3a25e233013d322ccbe688726f52cd6
rhaskia <rhaskiagamedev@gmail.com>
2024-08-17 16:17:15 +1200

    movement and camera movement

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,87 @@
-use bevy::prelude::*;
+use std::f32::consts::PI;
+
+use bevy::{prelude::*, input::mouse::MouseMotion};
+use bevy_rapier3d::prelude::*;
 
 #[derive(Component)]
 struct SoundPlane(Vec<Vec<Vec<f64>>>);
+
 #[derive(Component)]
 struct SoundImage;
 
+#[derive(Component, Default)]
+struct Player {
+    vertical_velocity: f32,
+    velocity: Vec3,
+    max_speed: f32,
+    acceleration: f32,
+    grounded: bool,
+}
+
 fn main() {
-    // u[0][40][40] = 0.0f64.sin();
-    // u[1][40][40] = 0.1f64.sin();
-
-
     App::new() 
         .add_plugins(DefaultPlugins)
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+        .add_plugins(RapierDebugRenderPlugin::default())
         .add_systems(Startup, start)
         .add_systems(Update, update)
+        .add_systems(Update, read_output)
+        .add_systems(Update, player_movement)
+        .add_systems(Update, camera_movement)
         .run();
 }
 
 fn start(
     mut commands: Commands,
-    mut textures: ResMut<Assets<Texture>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     let u = vec![vec![vec![0.0;80];80];2];
+    commands.spawn(SoundPlane(u));
 
-    let (width, height) = (256, 256);
-    let mut bytes = Vec::with_capacity(80 * 80 * 4);
-    for _y in 0..height {
-        for _x in 0..width {
-            bytes.push(0xff);
-            bytes.push(0x00);
-            bytes.push(0x00);
-            bytes.push(0xff);
-        }
-    }
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Circle::new(4.0)),
+        material: materials.add(Color::WHITE),
+        transform: Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
+        ..default()
+    }).insert(Collider::cuboid(4.0, 4.0, 0.1));
 
-    let texture = Texture::new(
-        Extent3d::new(width as u32, height as u32, 1),
-        TextureDimension::D2,
-        bytes,
-    );
+    commands
+        .spawn(RigidBody::KinematicPositionBased)
+        .insert(Collider::capsule_y(1.0, 0.5))
+        .insert(PbrBundle{
+            mesh: meshes.add(Capsule3d::new(0.5, 2.0)),
+            material: materials.add(Color::rgb_linear(1.0, 0.0, 0.0)),
+            transform: Transform::from_xyz(1.0, 3.0, 1.0),
+            ..default()
+        })
+        .insert(KinematicCharacterController {
+            snap_to_ground: Some(CharacterLength::Absolute(2.0)),
+            ..KinematicCharacterController::default()
+        })
+        .insert(GravityScale(1.0))
+        .insert(Player {
+            acceleration: 0.8,
+            max_speed: 5.0,
+            velocity: Vec3::new(0.0, 0.0, 0.0),
+            ..default()
+        });
 
-    let texture_handle = textures.add(texture);
-
-    commands.spawn(Camera2dBundle::default());
-    commands.spawn(SpriteBundle {
-        texture: texture_handle.clone(),
-        ..Default::default()
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
     });
+
+    // camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_rotation(Quat::from_rotation_y(PI)),
+        ..default()
+    });
+
 }
 
 fn update(
@@ -54,20 +89,64 @@ fn update(
     mut assets: Res<AssetServer>,
     mut query: Query<&mut Handle<Image>, With<SoundImage>>,
 ) {
-    let mut u = soundplane.get_single_mut().unwrap();
-    // if t < 1000 { u[t][40][60] = (t as f64 /10.0).sin(); }
-    // if t < 320 { u[t][t/4][20] = (t as f64 / 2.0).sin(); }
-    // draw_rect(&mut u[t], 20, 20, 35, 35);
-    let next = calculate_next_step(&u.0, 1.0, 0.125, 0.125, 0.025, 0.01);
-    u.0.push(next);
+    // let mut u = soundplane.get_single_mut().unwrap();
+    // let next = calculate_next_step(&u.0, 1.0, 0.125, 0.125, 0.025, 0.01);
+    // u.0.push(next);
 
-    let image = query.single_mut();
-    (*image)[0] = 1.0;
-    *image.dirty();
 
     //print!("{}", draw_screen(&u.0[t]));
 
     // std::thread::sleep(std::time::Duration::from_millis(30));
+}
+
+fn read_output(mut query: Query<(&KinematicCharacterControllerOutput, &mut Player)>) {
+    if let Ok((output, mut player)) = query.get_single_mut() {
+        player.grounded = output.grounded;
+    }
+}
+
+fn camera_movement(
+    mut camera_query: Query<&mut Transform, With<Camera3d>>,
+    mut player_query: Query<(&mut Transform, &Player), Without<Camera3d>>,
+    mut mouse_motion: EventReader<MouseMotion>,
+) {
+    let mut camera = camera_query.single_mut();
+    let mut player = player_query.single_mut().0;
+    for motion in mouse_motion.read() {
+        let yaw = -motion.delta.x * 0.003;
+        let pitch = -motion.delta.y * 0.002;
+        // Order of rotations is important, see <https://gamedev.stackexchange.com/a/136175/103059>
+        player.rotate_y(yaw);
+        camera.rotate_local_x(pitch);
+    }
+
+}
+
+fn player_movement(
+    mut query: Query<(&mut KinematicCharacterController, &mut Player)>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+) {
+    let (mut controller, mut player) = query.single_mut();
+
+    let horiz = get_key_axis(&keyboard_input, KeyCode::ArrowLeft, KeyCode::ArrowRight);
+    let vert = get_key_axis(&keyboard_input, KeyCode::ArrowDown, KeyCode::ArrowUp);
+    let new_velocity = Vec3::new(horiz, 0.0, vert).normalize_or_zero() * player.max_speed;
+    println!("{:?}", Vec3::new(horiz, 0.0, vert).normalize_or_zero());
+    // TODO seperate accel and decel
+    player.velocity = player.velocity.move_towards(new_velocity, player.acceleration);
+
+    if player.grounded {
+        player.vertical_velocity = 0.0;
+    } else {
+        player.vertical_velocity -= 9.8 * time.delta_seconds();
+    }
+
+    controller.translation = Some(Vec3::new(player.velocity.x, player.vertical_velocity, player.velocity.z) * time.delta_seconds());
+}
+
+fn get_key_axis(input: &Res<ButtonInput<KeyCode>>, neg: KeyCode, pos: KeyCode) -> f32 {
+    (input.pressed(pos) as i32 as f32) - (input.pressed(neg) as i32 as f32)
 }
 
 fn draw_screen(u: &Vec<Vec<f64>>) -> String {

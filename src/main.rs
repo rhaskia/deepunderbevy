5d6f1c3afec0f02dbbb1000db009197ea818981a
rhaskia <rhaskiagamedev@gmail.com>
2024-08-30 11:22:52 +1200

    move to terminal

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,76 @@
 mod input;
 mod modules;
 use std::f32::consts::PI;
-use bevy::render::render_asset::RenderAssetUsages;
-use bevy::render::render_resource::{TextureFormat, TextureDimension, Extent3d, TextureId};
-use bevy::window::{CursorGrabMode, PrimaryWindow};
-use bevy::{prelude::*, input::mouse::MouseMotion};
-use bevy_rapier3d::na::UnitVector3;
-use bevy_rapier3d::prelude::*;
-use bevy_lunex::prelude::*;
 use bevy::input::common_conditions::*;
 use input::{InputAxis, InputAxes};
+use bevy::prelude::*;
+use std::time::Duration;
+use bevy::{
+    app::{AppExit, ScheduleRunnerPlugin},
+    input::{keyboard::KeyboardInput, ButtonState},
+    prelude::*,
+};
+use bevy_ratatui::{
+    error::exit_on_error,
+    event::KeyEvent,
+    input_forwarding::{Capability, Detected, Emulate, EmulationPolicy, ReleaseKey},
+    kitty::KittyEnabled,
+    terminal::RatatuiContext,
+    RatatuiPlugins,
+};
+use crossterm::event::KeyEventKind;
+use ratatui::text::Text;
+use ratatui::prelude::Rect;
+use ratatui::widgets::Paragraph;
+use crossterm::event::KeyCode;
+use modules::Module;
+
+fn main() {
+    let wait_duration = std::time::Duration::from_secs_f64(1. / 60.); // 60 FPS
+    App::new()
+        .add_plugins(ScheduleRunnerPlugin::run_loop(wait_duration))
+        .add_systems(Startup, start)
+        .add_systems(Update, draw_scene_system.pipe(exit_on_error))
+        .add_systems(Update, player_movement)
+        .run();
+}
+
+#[derive(Resource, Deref, DerefMut)]
+struct LastKeypress(pub KeyEvent);
+
+#[derive(Resource, Deref, DerefMut)]
+struct LastBevyKeypress(pub KeyboardInput);
+
+#[derive(Resource, Deref, DerefMut)]
+struct BevyKeypresses(pub Vec<KeyCode>);
+
+fn draw_scene_system(
+    mut context: ResMut<RatatuiContext>,
+    player: Query<&Player>,
+    modules: Query<&Module>,
+) -> color_eyre::Result<()> {
+    let player = player.single();
+
+    context.draw(|frame| {
+        let width = frame.size().width / 2;
+        let height = frame.size().height / 2;
+
+        for module in &modules {
+            let x = (module.position.x * 6) - player.position.x + (width as i32);
+            let y = (module.position.y * 4) - player.position.y + (height as i32);
+            let rect = Rect::new(x as u16, y as u16, 7, 6);
+            let sprite = Paragraph::new("#-----#\n|     |\n|     |\n|     |\n#-----#");
+            frame.render_widget(sprite, rect);
+        }
+
+        let sprite = Paragraph::new("@");
+        let x = frame.size().width / 2;
+        let y = frame.size().height / 2;
+        frame.render_widget(sprite, Rect::new(x, y, 1, 1));
+    })?;
+
+    Ok(())
+}
 
 #[derive(Component)]
 struct SoundPlane(Vec<Vec<Vec<f32>>>);
@@ -19,11 +80,7 @@ struct SoundImage(Handle<Image>);
 
 #[derive(Component, Default)]
 struct Player {
-    vertical_velocity: f32,
-    velocity: Vec3,
-    max_speed: f32,
-    acceleration: f32,
-    grounded: bool,
+    position: IVec2,
 }
 
 #[derive(Component)]
@@ -32,212 +89,60 @@ struct SoundView;
 #[derive(Component)]
 struct SoundMat(Handle<StandardMaterial>);
 
-fn main() {
-    App::new() 
-        .add_plugins((DefaultPlugins, UiPlugin))
-        .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
-        // .add_plugins(RapierDebugRenderPlugin::default())
-        .add_systems(Startup, start)
-        .add_systems(Startup, cursor_grab)
-        .add_systems(Startup, spawn_ui)
-        .add_systems(Update, cursor_ungrab)
-        .add_systems(Update, cursor_grab.run_if(input_just_pressed(MouseButton::Left)))
-        .add_systems(Update, update)
-        .add_systems(Update, read_output)
-        .add_systems(Update, player_movement)
-        .add_systems(Update, camera_movement)
-        .run();
-}
-
 fn start(
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut images: ResMut<Assets<Image>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     let mut u = vec![vec![vec![0.0;80];80];2];
     u[1][40][40] = 1.0;
     commands.spawn(SoundPlane(u));
 
-    commands
-        .spawn(RigidBody::KinematicPositionBased)
-        .insert(Collider::capsule_y(1.0, 0.5))
-        .insert(PbrBundle{
-            mesh: meshes.add(Capsule3d::new(0.5, 2.0)),
-            material: materials.add(Color::srgb_from_array([1.0, 0.0, 0.0])),
-            transform: Transform::from_xyz(1.0, 3.0, 1.0),
-            ..default()
-        })
-        .insert(KinematicCharacterController {
-            snap_to_ground: Some(CharacterLength::Absolute(2.0)),
-            ..KinematicCharacterController::default()
-        })
-        .insert(Player {
-            acceleration: 0.8,
-            max_speed: 5.0,
-            velocity: Vec3::new(0.0, 0.0, 0.0),
-            ..default()
-        });
+    commands.spawn(Player::default());
 
-    modules::spawn_module(&mut commands, &mut meshes, &mut materials);
+    commands.spawn(Module::new(0,0));
+    commands.spawn(Module::new(1,0));
 
-    // camera
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_rotation(Quat::from_rotation_y(PI)),
-        ..default()
-    });
-
-    let texture = Image::new(
-        Extent3d { width: 80, height: 80, ..default() },
-        TextureDimension::D2,
-        vec![0xFF; 80 * 80 * 4],
-        TextureFormat::Rgba8Unorm,
-        RenderAssetUsages::RENDER_WORLD | RenderAssetUsages::MAIN_WORLD
-    );
-
-    let texture_handle = images.add(texture);
-
-    commands.spawn(SoundImage(texture_handle.clone()));
-    let mat = materials.add(
-        StandardMaterial {
-            base_color_texture: Some(texture_handle.clone()),
-            emissive_texture: Some(texture_handle),
-            emissive: Color::srgb(1.0, 1.0, 1.0).into(),
-            ..default()
-        }
-    );
-    commands.spawn(SoundMat(mat.clone()));
-
-    commands.spawn((
-        PbrBundle {
-            mesh: meshes.add(Plane3d::new(Vec3::new(0.0, 0.0, 1.0), Vec2::new(1.0, 1.0))),
-            material: mat,
-            transform: Transform::from_translation(Vec3::new(0.0, 0.0, -3.0)),
-            ..default()
-        },
-        SoundView
-    ));
-    
     commands.insert_resource(InputAxes::default());
-}
-
-
-fn cursor_grab(
-    mut q_cursor: Query<&mut Cursor2d>,
-) {
-    if let Ok(mut cursor) = q_cursor.get_single_mut() {
-        cursor.confined = true;
-        cursor.visible = true;
-    }
-}
-
-fn cursor_ungrab(
-    mut q_cursor: Query<&mut Cursor2d>,
-    keyboard_input: Res<ButtonInput<KeyCode>>,
-) {
-    if let Ok(mut cursor) = q_cursor.get_single_mut() {
-        if keyboard_input.pressed(KeyCode::Escape) {
-            cursor.confined = false;
-            cursor.visible = true;
-        } 
-    }
 }
 
 fn update(
     mut soundplane: Query<&mut SoundPlane>,
     mut query: Query<&mut SoundImage>,
     mut q_mat: Query<&mut SoundMat>,
-    mut images: ResMut<Assets<Image>>,
     mut commands: Commands,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    player_q: Query<&Transform, With<Player>>,
     time: Res<Time>,
 ) {
-    let mut u = soundplane.get_single_mut().unwrap();
-    u.0[1][40][40] = (time.elapsed().as_secs_f32() * 5.0).sin();
-    let next = calculate_next_step(&u.0, 1.0, 0.2, 0.2, 0.025, 0.0);
+    let mut u = soundplane.single_mut();
+    let mut player = player_q.single();
+    let pos = player.translation * 16.0;
+    u.0[1][(pos.z + 40.0) as usize][(pos.x + 40.0) as usize] = (time.elapsed().as_secs() as f32 * 2.0).sin();
+    let next = calculate_next_step(&u.0, 1.0, 0.125, 0.125, 0.025, 0.01);
     u.0[0] = u.0[1].clone();
     u.0[1] = next;
-    
-    let handle = &mut query.single_mut().0;
-    let texture = images.get_mut(&handle.clone()).unwrap();
-    for x in 0..80 {
-        for y in 0..80 {
-            let value = (u.0[u.0.len() - 1][x][y] * 255.0) as u8;
-            let blue = (u.0[u.0.len() - 1][x][y] * -255.0) as u8;
-            texture.data[(x * 80 + y) * 4] = blue;
-            texture.data[(x * 80 + y) * 4 + 1] = value;
-            texture.data[(x * 80 + y) * 4 + 2] = 0;
-        }
-    }
-    let mut mat = q_mat.get_single_mut().unwrap();
-
-    // materials.get_mut(&mut mat.0).unwrap().base_color = Color::srgb_from_array([time.elapsed().as_secs() as f32 / 10.0, 0.0, 0.0]);
-    materials.get_mut(&mut mat.0).unwrap().base_color_texture = Some(handle.clone());
-    materials.get_mut(&mut mat.0).unwrap().emissive_texture = Some(handle.clone());
-
-    // std::thread::sleep(std::time::Duration::from_millis(30));
-}
-
-fn read_output(mut query: Query<(&KinematicCharacterControllerOutput, &mut Player)>) {
-    if let Ok((output, mut player)) = query.get_single_mut() {
-        player.grounded = output.grounded;
-    }
-}
-
-fn camera_movement(
-    mut camera_query: Query<&mut Transform, With<Camera3d>>,
-    mut player_query: Query<(&mut Transform, &Player), Without<Camera3d>>,
-    mut mouse_motion: EventReader<MouseMotion>,
-    mut q_windows: Query<&mut Window, With<PrimaryWindow>>,
-) {
-    let primary_window = q_windows.single_mut();
-
-    let mut camera = camera_query.single_mut();
-    let mut player = player_query.single_mut().0;
-
-    camera.translation = player.translation + Vec3::new(0.0, 1.0, 0.0);
-
-    // Only rotate the camera when cursor is grabbed
-    //;if primary_window.cursor.grab_mode == CursorGrabMode::None { return }
-
-    for motion in mouse_motion.read() {
-        let yaw = -motion.delta.x * 0.003;
-        let pitch = -motion.delta.y * 0.002;
-        // Order of rotations is important, see <https://gamedev.stackexchange.com/a/136175/103059>
-        player.rotate_y(yaw);
-        camera.rotate_y(yaw);
-        camera.rotate_local_x(pitch);
-    }
 }
 
 fn player_movement(
-    mut query: Query<(&mut KinematicCharacterController, &mut Player, &Transform)>,
-    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<&mut Player>,
+    mut events: EventReader<KeyEvent>,
+    mut exit: EventWriter<AppExit>,
     axes: Res<InputAxes>,
     time: Res<Time>,
 ) {
-    let (mut controller, mut player, transform) = query.single_mut();
+    let mut player = query.single_mut();
 
-    let horiz = get_key_axis(&keyboard_input, axes.get("Horizontal"));
-    let forward = get_key_axis(&keyboard_input, axes.get("Vertical"));
-    let move_vector = forward * transform.forward() + horiz * transform.left();
-    let new_velocity = move_vector.normalize_or_zero() * player.max_speed;
-    // TODO seperate accel and decel
-    player.velocity = player.velocity.move_towards(new_velocity, player.acceleration);
-
-    if player.grounded {
-        player.vertical_velocity = 0.0;
-    } else {
-        player.vertical_velocity -= 9.8 * time.delta_seconds();
+    for event in events.read() {
+        use KeyCode::*;
+        if event.kind != KeyEventKind::Press { continue; }
+        match event.code {
+            Char('w') => player.position.y -= 1, 
+            Char('a') => player.position.x -= 1,
+            Char('s') => player.position.y += 1, 
+            Char('d') => player.position.x += 1,
+            Char('q') => { exit.send_default(); },
+            _ => {}
+        }
     }
-
-    controller.translation = Some(Vec3::new(player.velocity.x, player.vertical_velocity, player.velocity.z) * time.delta_seconds());
-}
-
-fn get_key_axis(input: &Res<ButtonInput<KeyCode>>, axis: &InputAxis) -> f32 {
-    let pos = axis.pos.iter().map(|key| input.pressed(*key)).any(|p| p);
-    let neg = axis.neg.iter().map(|key| input.pressed(*key)).any(|p| p);
-    (neg as i32 as f32) - (pos as i32 as f32)
 }
 
 fn draw_screen(u: &Vec<Vec<f32>>) -> String {
@@ -271,61 +176,20 @@ fn calculate_next_step(u: &Vec<Vec<Vec<f32>>>, c: f32, dx: f32, dy: f32, dt: f32
             new_state[x][y] = ((c*c) * (dt*dt) * (dudy + dudx))
                             + (2.0*u[t][x][y]) - u[t-1][x][y]
                             - friction;
+
+            if x < 5 || x > 76 || y < 5 || y > 76 {
+                new_state[x][y] *= 0.99;
+            }
         }
     }
 
     new_state
 }
 
-fn draw_rect(u: &mut Vec<Vec<f32>>, value: f32, sx: usize, sy: usize, ex: usize, ey: usize) {
+fn draw_rect_sim(u: &mut Vec<Vec<f32>>, value: f32, sx: usize, sy: usize, ex: usize, ey: usize) {
     for x in sx..ex {
         for y in sy..ey {
             u[x][y] = value;
         }
     }
-}
-
-fn spawn_ui(
-    mut commands: Commands,
-    mut material: ResMut<Assets<StandardMaterial>>,
-    assets: Res<AssetServer>,
-) {
-    // Spawn cursor
-    commands.spawn(CursorBundle::default());
-
-
-    // Spawn the floating Ui panel
-    commands.spawn((
-        UiTreeBundle::<MainUi> {
-            transform: Transform::from_xyz(0.4, 0.3, 0.3),
-            tree: UiTree::new3d("PanelWidget"),
-            ..default()
-        },
-    )).with_children(|ui| {
-        ui.spawn((
-            // Link this widget
-            UiLink::<MainUi>::path("Menu/Button"),
-
-            // The layout that is used when in base state
-            UiLayout::window_full().size((818.0, 965.0)).pack::<Base>(),
-
-            // // Give the mesh an image
-            // UiMaterial3dBundle::from_transparent_image(&mut material, texture_handle),
-            UiColor::<Base>::new(Color::srgb_from_array([1.0, 0.0, 0.0])),
-            UiColor::<Hover>::new(Color::srgb_from_array([1.0, 1.0, 0.0])),
-
-            // Make the panel pickable
-            PickableBundle::default(),
-
-            // This is required to control our hover animation
-            UiAnimator::<Hover>::new().forward_speed(6.0).backward_speed(5.0),
-
-            // This is required for Layout animation
-            UiLayoutController::default(),
-
-            // The layout that is used when in hover state
-            UiLayout::window_full().x(100.0).size((818.0, 965.0)).pack::<Hover>(),
-            UiClickEmitter::SELF,
-        ));
-    }); 
 }
--- src/modules.rs
+++ src/modules.rs
@@ -1,5 +1,4 @@
 use bevy::prelude::*;
-use bevy_rapier3d::prelude::*;
 
 #[derive(Component)]
 enum Dir {
@@ -27,52 +26,13 @@ struct Door {
     position: Vec3,
 }
 
-pub fn spawn_module(
-    commands: &mut Commands,
-    meshes: &mut ResMut<Assets<Mesh>>,
-    materials: &mut ResMut<Assets<StandardMaterial>>,
-) {
-    // Floor
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Cuboid::new(6.0, 0.2, 6.0)),
-        material: materials.add(Color::WHITE),
-        transform: Transform::from_xyz(0.0, -3.0, 0.0),
-        ..default()
-    }).insert(Collider::cuboid(3.0, 0.1, 3.0));
+#[derive(Component)]
+pub struct Module {
+    pub position: IVec2,
+}
 
-    // Roof
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Cuboid::new(6.0, 0.2, 6.0)),
-        material: materials.add(Color::WHITE),
-        transform: Transform::from_xyz(0.0, 3.0, 0.0),
-        ..default()
-    }).insert(Collider::cuboid(3.0, 0.1, 3.0));
-
-    for dir in [0.0] {
-        // let dir_v = dir.to_vec();
-        // let x = dir_v.z.abs() * 5.8 + 0.2;
-        // let z = dir_v.x.abs() * 5.8 + 0.2;
-        //
-        // commands.spawn(PbrBundle {
-        //     mesh: meshes.add(Cuboid::new(x, 6.0, z)),
-        //     material: materials.add(Color::WHITE),
-        //     transform: Transform::from_xyz(dir_v.x * 3.0, 0.0, dir_v.z * 3.0),
-        //     ..default()
-        // })
-        // .insert(Door { open: false, position: dir.to_vec() })
-        // .insert(Collider::cuboid(x / 2.0, 3.0, z / 2.0));
+impl Module {
+    pub fn new(x: i32, y: i32) -> Self {
+        Module { position: IVec2::new(x, y) }
     }
-
-    commands.spawn(PointLightBundle {
-        point_light: PointLight {
-            shadows_enabled: true,
-            intensity: 300_000.0,
-            color: Color::srgb_from_array([1.0, 1.0, 0.8]),
-            range: 7.0,
-            radius: 0.6,
-            ..default()
-        },
-        transform: Transform::from_xyz(0.0, 1.8, 0.0),
-        ..default()
-    });
 }
